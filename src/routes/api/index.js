726784a78e2a7c2e207389cba9ce8b6032daf84c
Jae Bradley <jae.b.bradley@gmail.com>
2019-03-02 13:37:15 -0800

    Add review statistics endpoint

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,7 @@ import Router from 'express-promise-router';
 
 import handleUser from 'Src/requests/api/handleUser';
 import handleShelves from 'Src/requests/api/handleShelves';
+import handleReviewStatistics from 'Src/requests/api/handleReviewStatistics';
 
 import authentication from './authentication';
 
@@ -11,5 +12,6 @@ router.use('/authentication', authentication);
 
 router.get('/user', handleUser);
 router.get('/user/shelves', handleShelves);
+router.get('/book/review_statistics', handleReviewStatistics);
 
 export default router;
